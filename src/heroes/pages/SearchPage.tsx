d9bbf40d2bd89793058218af14584b0a1dbd2e17
pks2828 <danieldoriamoncada28@gmail.com>
2024-03-07 16:14:49 -0600

    final seccion 14

--- src/heroes/pages/SearchPage.tsx
+++ src/heroes/pages/SearchPage.tsx
@@ -1,13 +1,37 @@
-import { HeroCard } from '../components/HeroCard';
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+
+import { useForm } from "../../hooks/useForm";
+import { getHeroesByName } from "../helpers";
+import { HeroCard } from "../components";
 
 export const SearchPage = () => {
+  const navigate = useNavigate(); // Hook para navegar entre rutas
+  const location = useLocation(); // Hook para la informacion de la ruta actual
+
+  const { q = "" } = queryString.parse(location.search);
+  const heroes = getHeroesByName(q as string); // Obtenemos los heroes que coincidan con la busqueda
+  
+  const showSearch:boolean = (q!.length === 0); // Mostramos el formulario si no hay busqueda o si la busqueda es vacia 
+  const showError:boolean = (q!.length > 0) && heroes.length === 0 ; // Mostramos el formulario si no hay busqueda o si la busqueda es vacia 
+
+  //Hook useForm para el manejo de formularios
+  const { searchText, onInputChange } = useForm({
+    searchText: q as string,
+  });
+
+  const onSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // if (searchText.trim().length <= 1) return;
+    navigate(`?q=${searchText}`);
+  };
+
   return (
     <>
       <h1>Search</h1>
       <hr />
 
       <div className="row">
-
         <div className="col-5">
           <h4>Searching</h4>
           <hr />
@@ -41,10 +65,7 @@ export const SearchPage = () => {
           {/* <HeroCard /> */}
 
         </div>
-      
       </div>
-      
-
     </>
   )
 }
